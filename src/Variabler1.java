47e1c18b855187d0c925b9e4899699d8a4bb56f3
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-15 10:29:19 +0100

    1. variabler

--- src/Variabler1.java
+++ src/Variabler1.java
@@ -1,5 +1,9 @@
 public class Variabler1 {
-    
+    public static void main(String[] args) {
+        //Vis et eksempel på typecasting fra double til int.
+        int randomTal = (int) (Math.random()*6);
+        System.out.println(randomTal);
+    }
 
 
 }
