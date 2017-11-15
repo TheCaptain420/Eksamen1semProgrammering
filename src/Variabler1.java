1171013c6ccb159cbb1f35d86415e9bbd6721b23
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-15 10:43:53 +0100

    2

--- src/Variabler1.java
+++ src/Variabler1.java
@@ -1,9 +1,17 @@
 public class Variabler1 {
     public static void main(String[] args) {
         //Vis et eksempel på typecasting fra double til int.
-        int randomTal = (int) (Math.random()*6);
+        int randomTal = (int) (Math.random()*6); //laver et random tal
         System.out.println(randomTal);
     }
 
+
+    /*BRUGES TIL KLASSEROGOBJEKTER2 !!!*/
+    Variabler1(String skrivNoget){
+        //skrivNoget kunne f.eks. så printes her, eller ligges i arraylist.
+    }
+    public int testMetode2plus2(){
+        return 2+2;
+    }
 
 }
