418032458c2373438a9b31ad62d1ada998c71e8c
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-12-15 10:43:41 +0100

    7

--- src/Arrays5.java
+++ src/Arrays5.java
@@ -1,11 +1,13 @@
 public class Arrays5 {
     public static void main(String[] args) {
         int[] arraytest = new int[10];
+        //for loop til indsættelse
         for (int k = 0; k <= 9; k++){
             int randomTal = (int) (Math.random()*6); //laver et random tal
             arraytest[k]=randomTal; // giver værdien ind på
             System.out.println("Array "+k+" har fået værdien: "+ randomTal); //printer it
         }
+        //array [5] får værdien 80
         arraytest[5] = 80;
         System.out.println("Array 5 har fået ny værdi : "+ arraytest[5]);
 
