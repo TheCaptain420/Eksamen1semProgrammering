325b22fea9d597a346ccefa2129bde9c8f1dc089
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-12-15 10:51:55 +0100

    8

--- src/ArrayList6.java
+++ src/ArrayList6.java
@@ -6,16 +6,17 @@ public class ArrayList6 {
         ArrayList<Integer> obj = new ArrayList<>();
 
         //tilføjer
-        obj.add(2);
-        obj.add(54);
-        obj.add(2,5);
+        obj.add(2); //plads 0 = 2
+        obj.add(54);//plads 1 = 54
+        obj.add(2,5); //plads 2 = 5
         System.out.println(obj);
+        System.out.println(obj.get(1));//printer plads 1
 
         //remover INDEX number
         obj.remove(0);
         System.out.println(obj);
 
-        //
+        //Fjerner alt i arraylist
         obj.clear();
         System.out.println(obj);
 
